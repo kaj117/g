339103bfdacc55887ab31f50f61d59418db6d317
kaj117 <70688258+kaj117@users.noreply.github.com>
2020-09-04 21:03:05 +0530

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1302270529:AAFt0TLSof51vhGODA21i9NqKSjk7PWT2D8"
+  APP_ID = 13838450
+  API_HASH = "e3d2c299cc3c5cc26c283cecd2eb97c"
+  AUTH_CHANNEL = [-1001269473865]
